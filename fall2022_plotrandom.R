878017b054555aaf741fe0e6c89ed85d2d1ff369
mitchellthornton <112967080+mitchellthornton@users.noreply.github.com>
2022-10-14 15:46:36 -0400

    Add files via upload

---
+++ fall2022_plotrandom.R
@@ -0,0 +1,33 @@
+## PlotRandom.R
+##
+## Purpose: a function that will generate a random sample of normal deviates,
+##          plot a histogram and maybe provide some "other stuff"
+
+
+## Stuff from last time:
+plotRandomD <- function(numpts=100) {
+  hist(rnorm(numpts))
+}
+
+plotRandomD()
+
+
+###################################
+
+## Let's build in more flexibility for our function:
+plotRandomNormals <- function(numpts=1000, mu=24, sigma=3, numbins=15,
+                              title = "My Histogram, Oct 14 2022", 
+                              meanColor="red", seed=8675309) {
+  set.seed(seed)
+  rand_x <- rnorm(numpts, mean=mu, sd=sigma)
+  mean_x <- mean(rand_x)
+  hist(rand_x, breaks=numbins, main=title)
+  abline(v=mean_x, col=meanColor)
+  list(Mean_X = mean_x,
+       SD_X = sd(rand_x),
+       RandomValues = rand_x)
+}
+
+plotRandomNormals()
+
+
